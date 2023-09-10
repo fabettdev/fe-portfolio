2b2fcd93447b67c631ea28d07ee3703dcc21b160
fabett <fabrizioettori@gmail.com>
2023-09-10 16:49:33 +0200

    added skills section

---
+++ src/components/sections/Skills.tsx
@@ -0,0 +1,21 @@
+export default function Skills(): React.ReactElement {
+  return (
+    <section className="text-center">
+      <h2 className="mb-48">Competenze</h2>
+
+      <div className="flex flex-col font-black text-9xl gap-20">
+        <div>HTML</div>
+        <div>CSS/Sass</div>
+        <div>JavaScript (ES6)</div>
+        <div>TypeScript</div>
+        <div>React</div>
+        <div>React Native</div>
+        <div>Next.js</div>
+        <div>Expo</div>
+        <div>Angular</div>
+        <div>SQL</div>
+        <div>Git</div>
+      </div>
+    </section>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,16 @@
 // Components
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import Hero from "../components/Hero";
+import Hero from "../components/sections/Hero";
+import Skills from "../components/sections/Skills";
 
 function Home() {
   return (
     <>
       <Header />
-      <main className="bg-white text-black h-[200vh]">
+      <main className="bg-white text-black pb-48">
         <Hero />
+        <Skills />
       </main>
       <div className="h-[30vh]"></div>
       <Footer />
